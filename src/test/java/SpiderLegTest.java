0f26d0f12ecb58e7f026f9a6089aa22b2cad2c48
Tinchev,Rostislav R.I <I358097@fhict.nl>
2019-05-05 20:42:16 +0200

    ADD SpiderLeg empty class, SpiderLegTest class with TODOs for every method needed and the constructor, dependencies and auto build added

---
+++ src/test/java/SpiderLegTest.java
@@ -0,0 +1,33 @@
+import junitparams.JUnitParamsRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import junitparams.Parameters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+The test class for the SpiderLeg class that checks if the methods are behaving properly.
+ */
+public class SpiderLegTest {
+    //TODO SpiderLeg constructor
+    //1.Instantiate a new linked list
+    //2.Set variable for the user agent
+
+    //TODO searchForWord() method:
+    //1.Accept a word parameter as String
+    //2.Return the body of the HTML doc where the word was found
+    // - As lower case
+    //3.Check if the body is empty
+
+    //TODO getLinks() method:
+    // - Returns the lists found by the searchForWord() method.
+
+    //TODO crawl() method:
+    //1.Connect to url
+    //2.Get connection doc
+    //3.Check if connection established, else return false
+    //4.If document is empty return false
+    //5.Retrieve all of the links on the webpage
+    //6.Add each link to a list
+}
---
+++ src/test/java/SpiderTest.java
@@ -0,0 +1,2 @@
+public class SpiderTest {
+}
