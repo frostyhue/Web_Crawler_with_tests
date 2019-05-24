a0a645f564d03d8695d1d3853b6abaa35343fe22
RTinchev <rostislav.tinchev@gmail.com>
2019-05-24 23:36:53 +0200

    ADD variables needed for Spider to function properly and respective getters, TODO half-way complete

--- src/main/java/Spider.java
+++ src/main/java/Spider.java
@@ -1,12 +1,34 @@
+import java.util.List;
+import java.util.Set;
+
 public class Spider {
     //TODO add functionality to Spider class:
-    //1.Variable containing the max number of pages for search and create a getter method for it.
-    //2.Variable containing the pages already visited by the search algorithm and create a getter method for it.
-    //3.Variable containing the pages to yet be visited by the search algorithm and create a getter method for it.
-    //4.Variable containing the url that the sought word was found in and create a getter method for it.
+    //1.Variable containing the max number of pages for search and create a getter method for it. (DONE)
+    //2.Variable containing the pages already visited by the search algorithm and create a getter method for it. (DONE)
+    //3.Variable containing the pages to yet be visited by the search algorithm and create a getter method for it. (DONE)
+    //4.Variable containing the url that the sought word was found in and create a getter method for it. (DONE)
     //5.Constructor declaring all of the variables.
     //6.Search method for finding the word sought.
     //7.Method retrieving the next url in line.
 
-    
+    private int Max_Number_Of_Pages;
+    private Set<String> pagesVisited;
+    private List<String> pagesToVisit;
+    private String urlFound;
+
+    public String getUrlFound() {
+        return urlFound;
+    }
+
+    public List<String> getPagesToVisit() {
+        return pagesToVisit;
+    }
+
+    public Set<String> getPagesVisited() {
+        return pagesVisited;
+    }
+
+    public int getMax_Number_Of_Pages() {
+        return Max_Number_Of_Pages;
+    }
 }
