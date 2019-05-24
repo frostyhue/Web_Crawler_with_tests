42dec28ef8a7cb9fe39349cc58cc2f7331962a8c
RTinchev <rostislav.tinchev@gmail.com>
2019-05-24 23:38:24 +0200

    ADD TODO to SpiderTest class for each block needed to be tested in Spider

--- src/test/java/SpiderTest.java
+++ src/test/java/SpiderTest.java
@@ -4,6 +4,24 @@ import junitparams.JUnitParamsRunner;
  * The test class for the Spider class that checks if the methods are behaving properly.
  */
 public class SpiderTest {
+//TODO constructor functionality tests
+    //1.Initialize max-number-of-pages to a reasonable number, e.g. 20.
+    //2.Instantiate pagesVisited as a HashSet<String>.
+    //3.Instantiate pagesToVisit as a LinkedList<String>.
+    //4.Initialize urlFound as an empty string.
+    //5.Instantiate leg as SpiderLeg object.
 
+    //TODO search() method functionality tests
+    //1.Populate pagesToVisit using leg.crawl() method.
+    //2.Loop through each url and use leg.searchForWord() to find the sought word.
+    //3.Set url that should be checked first.
+    //4.If word is found, break loop and return the url.
+    //5.If not, continue checking urls.
+    //6.Check if pagesToVisit is empty.
+
+    //TODO getNextURL() method functionality tests:
+    //1.Return the next url in line from pagesToVisit.
+    //2.Add the previous url to pagesVisited.
+    //3.Check if the url has been crawled yet.
 
 }
