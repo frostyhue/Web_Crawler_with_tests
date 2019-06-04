3a66cfd32633ecb6aa4f23e82f03cc7106336e19
TeodorGenov <teodor.genov23@gmail.com>
2019-06-04 23:13:47 +0200

    ADD Test to class to check if the list of visited pages is a hash set

--- src/test/java/SpiderTest.java
+++ src/test/java/SpiderTest.java
@@ -1,8 +1,15 @@
 import org.junit.Test;
 
+import java.util.HashSet;
+
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class SpiderTest {
+
+    /**
+     * Test to check if the max number of pages is set to 10
+     */
     @Test
     public void assertIfMaxNumberOfPagesIsSetTo()
     {
@@ -11,5 +18,15 @@ public class SpiderTest {
         //act
         //assert
         assertEquals(10, spider.getMax_number_of_pages());
+    }
+
+    @Test
+    public void assertIfPagesVisitedIsAHashSet()
+    {
+        //arrange
+        Spider spider = new Spider();
+        //act
+        //assert
+        assertTrue(spider.getPagesToVisit() instanceof HashSet);
     }
 }
