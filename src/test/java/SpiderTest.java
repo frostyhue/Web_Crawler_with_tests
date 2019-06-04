bece00724962024545ed7c8307fba8d0194d20cd
TeodorGenov <teodor.genov23@gmail.com>
2019-06-04 23:04:51 +0200

    ADD Test to SpiderTest class checking if number is = 20

--- src/test/java/SpiderTest.java
+++ src/test/java/SpiderTest.java
@@ -1,2 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SpiderTest {
+    @Test
+    public void assertIfMaxNumberOfPagesIsSetTo()
+    {
+        //arrange
+        Spider spider = new Spider();
+        //act
+        //assert
+        assertEquals(20, spider.getMax_number_of_pages());
+    }
 }
