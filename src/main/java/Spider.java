03d702358a5cfbc9f11297a58b88af7203513fd2
TeodorGenov <teodor.genov23@gmail.com>
2019-06-04 22:57:38 +0200

    ADD Vars needed for the Spider class to function normally

--- src/main/java/Spider.java
+++ src/main/java/Spider.java
@@ -1,3 +1,29 @@
+import java.util.List;
+import java.util.Set;
+
 public class Spider {
+    private int max_number_of_pages;
+    private Set<String> pagesVisited;
+    private List<String> pagesToVisit;
+    private String URLFound;
+
+    public int getMax_number_of_pages()
+    {
+        return this.max_number_of_pages;
+    }
+
+    public String getURLFound(){
+        return this.URLFound;
+    }
+
+    public Set<String> getPagesVisited()
+    {
+        return this.pagesVisited;
+    }
+
+    public List<String> getPagesToVisit()
+    {
+        return this.pagesToVisit;
+    }
 
 }
