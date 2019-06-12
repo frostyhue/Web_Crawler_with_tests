fc3fdd7786e925e2cff6b7f15e6241029b2ec72c
TeodorGenov <teodor.genov23@gmail.com>
2019-06-12 12:31:53 +0200

    ADD Test to check if the getter for the artist works. Test succeeds. Now refactored including all the variables.

--- src/main/java/Music.java
+++ src/main/java/Music.java
@@ -1,20 +1,28 @@
 public class Music {
     /**
      * TODO:
-     * 1. Variable containing name of music
-     * 2. Variable containing format
-     * 3. Variable containing year of debut
-     * 4. Variable containing artist
-     * 5. Variable containing the category
-     * 6. Constructor for declaring all the variables
+     * 1. Variable containing name of music - done
+     * 2. Variable containing format - done
+     * 3. Variable containing year of debut - done
+     * 4. Variable containing artist - done
+     * 5. Variable containing the category - done
+     * 6. Constructor for declaring all the variables - done
      * 7. Method for making the object into JSONObject
      */
 
+    private String title;
     private String artist;
+    private String format;
+    private String year;
+    private String category;
 
-    public Music(String artist)
+    public Music(String title, String artist, String format, String year, String category)
     {
+        this.title = title;
         this.artist = artist;
+        this.format = format;
+        this.year = year;
+        this.category = category;
     }
 
     public String getArtist()
