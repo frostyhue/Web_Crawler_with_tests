b7463326a6b0de09afbe334443c08ebb405604a8
TeodorGenov <teodor.genov23@gmail.com>
2019-06-12 22:10:11 +0200

    ADD Test for checking if book title is instantiated. Test Succeeds

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,21 @@
 public class Book {
+    /** TODO:
+     * 1. Variable for book title
+     * 2. Variable for book category
+     * 3. Variable for book genre
+     * 4. Variable for format
+     * 5. Variable for year
+     * 6. Variable for authors
+     * 7. Variable for publisher
+     * 8. Variable for isbn
+     * 9. Constructor for the book object
+     * 10. Method for transforming to JSON
+     */
+
+   public String title;
+
+   public Book(String title)
+   {
+       this.title = title;
+   }
 }
