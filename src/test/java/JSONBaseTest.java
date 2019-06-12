230f77804ae9b979bb39314c099d4adb2722798c
RTinchev <rostislav.tinchev@gmail.com>
2019-06-12 18:54:38 +0200

    Test method added checking if JSONBase class is abstract, test fails

--- src/test/java/JSONBaseTest.java
+++ src/test/java/JSONBaseTest.java
@@ -1,2 +1,21 @@
+import junitparams.JUnitParamsRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import junitparams.Parameters;
+
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.*;
+
 public class JSONBaseTest {
+
+    /**
+     * Checking if JSONBase class is abstract.
+     */
+    @Test
+    public void assertJSONBase_IsAbstractClass(){
+        boolean isAbstract = Modifier.isAbstract(JSONBase.class.getModifiers());
+
+        assertTrue(isAbstract);
+    }
 }
