bc0ab8156dab1ab761be74ca7d32816633cde35c
RTinchev <rostislav.tinchev@gmail.com>
2019-06-12 19:32:56 +0200

    Genre parameter and getter added to JSONBase class, tests pass

--- src/main/java/JSONBase.java
+++ src/main/java/JSONBase.java
@@ -1,7 +1,12 @@
 public abstract class JSONBase {
     private String year;
+    private String genre;
 
     public String getYear() {
         return year;
+    }
+
+    public String getGenre() {
+        return genre;
     }
 }
