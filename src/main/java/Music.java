3777d79640067902b8965388fb93e98622ba3a22
TeodorGenov <teodor.genov23@gmail.com>
2019-06-12 12:05:03 +0200

    ADD TODO for the Music class and MusicTest class.

---
+++ src/main/java/Music.java
@@ -0,0 +1,14 @@
+public class Music {
+    /**
+     * TODO:
+     * 1. Variable containing name of music
+     * 2. Variable containing format
+     * 3. Variable containing year of debut
+     * 4. Variable containing artist
+     * 5. Variable containing the category
+     * 6. Constructor for declaring all the variables
+     * 7. Method for making the object into JSONObject
+     */
+
+
+}
