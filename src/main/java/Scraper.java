aff4c28116e991dbeba31b17414e902cb850bd88
RTinchev <rostislav.tinchev@gmail.com>
2019-06-13 14:15:31 +0200

    Functionality added to Scraper, test passes

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,2 +1,13 @@
 public class Scraper {
+    private String url;
+    private Spider spider;
+
+    Scraper(String url, String word){
+        this.spider = new Spider();
+        this.url = spider.search(url, word);
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
