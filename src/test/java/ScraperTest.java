48663f77e966970605765cbd65645121979fe511
RTinchev <rostislav.tinchev@gmail.com>
2019-06-13 14:11:28 +0200

    Test method added checking if url of Scraper object is set with the expected result, test fails

--- src/test/java/ScraperTest.java
+++ src/test/java/ScraperTest.java
@@ -1,2 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ScraperTest {
+    /**
+     * Method asserts if when an obj of type Scraper is instantiated, the url of the object is returned by the Spider.
+     */
+    @Test
+    public void assertIf_WhenScraperObjInstantiated_UrlIsSet(){
+        String url = "http://i358097.hera.fhict.nl/";
+        String word = "Lord of The Rings";
+        Scraper scraper = new Scraper(url, word);
+
+        assertEquals(scraper.getUrl(), "http://i358097.hera.fhict.nl/details.php?id=203");
+    }
 }
