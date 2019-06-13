4ed638b5ebf9f93fc4fb7f26820b02932df3a2c8
RTinchev <rostislav.tinchev@gmail.com>
2019-06-13 12:46:09 +0200

    Test method added to MovieTest that checks if Movie object is instance of JSONBase, test fails

--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -1,2 +1,24 @@
+import junitparams.JUnitParamsRunner;
+import org.json.simple.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import junitparams.Parameters;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.*;
+
 public class MovieTest {
+    /**
+     * Checks if Movie inherits from JSONBase.
+     */
+    @Test
+    public void asserIf_MovieObject_inheritsFromJSONBase(){
+        Movie movieObj = new Movie();
+
+        assertTrue(movieObj instanceof JSONBase);
+    }
+
 }
