f427629cef55592cf221c9795ccc5b9dee5f3146
RTinchev <rostislav.tinchev@gmail.com>
2019-06-16 12:54:45 +0200

    API class improvements, resources can be passed now.

--- src/main/java/MovieResources.java
+++ src/main/java/MovieResources.java
@@ -1,21 +1,20 @@
+import org.json.simple.JSONObject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Path("uni")
 public class MovieResources {
 
-    private List<Movie> movieList = new ArrayList<>();
+    private List<JSONObject> movieList = new ArrayList<>();
     private List<String> actors = new ArrayList<>();
     private List<String> writers = new ArrayList<>();
-
-    public MovieResources()
-    {
-        movieList.add(new Movie("genre", "format", "year", "title", "director", "category", writers, actors ));
-    }
 
     @GET
     @Path("movies")
@@ -26,10 +25,18 @@ public class MovieResources {
     }
 
     @GET
-    @Path("movieJson")
+    @Path("movieJson/{title}")
     @Produces({MediaType.APPLICATION_JSON})
-    public Movie getMovieJson()
+    public JSONObject getMovieJson(@PathParam("title") String title)
     {
+        JSONObject result;
+        Scraper scraper = new Scraper("http://i358097.hera.fhict.nl/", title);
+        try {
+            result = scraper.getResultAsJSON();
+            movieList.add(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         if(movieList.size() > 0)
         {
             return movieList.get(0);
